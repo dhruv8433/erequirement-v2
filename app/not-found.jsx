1a187f7d50cef31462c5d2bf1a557f730c8ad7e6
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-08-07 21:16:45 +0530

    root layout

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <html>
+      <body className="flex justify-center items-center h-screen w-screen">
+        <h1 className="font-semibold">404 Not Found!!!</h1>
+        
+      </body>
+    </html>
+  );
+}
