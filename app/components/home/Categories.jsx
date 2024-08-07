7c26a44b1eee32f2bb5fe7253db8dd6a3b102d6c
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-08-07 23:03:53 +0530

    styling

--- app/components/home/Categories.jsx
+++ app/components/home/Categories.jsx
@@ -27,38 +27,65 @@ const Categories = () => {
     getHomeScreenData();
   }, []);
   return (
-    <div className="flex gap-4">
-      {loading ? (
-        <div className="flex gap-12">
-          {Array.of(1, 2, 3, 4, 5).map(() => (
-            <MySkeleton className="rounded" variant="rectangular" height={160} width={260} />
-          ))}
-        </div>
-      ) : (
-        <Swiper
-          slidesPerView={5}
-          spaceBetween={30}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Pagination, Navigation]}
-          className="mySwiper"
-        >
-          {data.map((category) => (
-            <SwiperSlide key={category.id}>
-              <div className="border-2 border-gray-700 p-3 rounded-2xl text-center hover:bg-white">
-                <img
-                  src={category.image}
-                  alt={category.name}
-                  className="object-contain aspect-square h-[120px] w-full"
-                />
-                <h1>{category.name}</h1>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      )}
+    <div className="my-4">
+      <h1 className="my-4 text-2xl font-semibold">Categories</h1>
+      <div className="flex gap-4">
+        {loading ? (
+          <div className="flex gap-12">
+            {Array.of(1, 2, 3, 4, 5).map(() => (
+              <MySkeleton
+                className="rounded"
+                variant="rectangular"
+                height={160}
+                width={260}
+              />
+            ))}
+          </div>
+        ) : (
+          <Swiper
+            slidesPerView={5}
+            spaceBetween={30}
+            navigation={true}
+            modules={[Navigation]}
+            breakpoints={{
+              300: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              440: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              640: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+              },
+              840: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 5,
+                spaceBetween: 50,
+              },
+            }}
+            className="mySwiper"
+          >
+            {data.map((category) => (
+              <SwiperSlide key={category.id}>
+                <div className="border-2 border-gray-700 p-3 rounded-2xl text-center hover:bg-white">
+                  <img
+                    src={category.image}
+                    alt={category.name}
+                    className="object-contain aspect-square h-[120px] w-full"
+                  />
+                  <h1>{category.name}</h1>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
+      </div>
     </div>
   );
 };
