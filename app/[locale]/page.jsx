67994b06201689b62fcfee5f40fde488a030ec47
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-08-08 16:58:12 +0530

    home page

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,13 +1,36 @@
-import { useTranslations } from "next-intl";
+"use client";
+
 import Categories from "../components/home/Categories";
+import Slider from "../components/home/Slider";
+import { useEffect, useState } from "react";
+import { getHomeScreen } from "../utils/HomeScreen";
+import TopProviders from "../components/home/TopProviders";
 
 export default function Home() {
-  const t = useTranslations("Index");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function getHomeScreenData() {
+    try {
+      const response = await getHomeScreen();
+      setLoading(false);
+      setData(response);
+    } catch (error) {
+      console.log("error: " + error);
+    }
+  }
+
+  useEffect(() => {
+    getHomeScreenData();
+  }, []);
+
   return (
     <div>
       {/* Categories */}
       <div className="my-4">
-        <Categories />
+        <Categories data={data.categorys} loading={loading} />
+        <Slider data={data.swiper} loading={loading} />
+        <TopProviders data={data.top_categories} loading={loading} />
       </div>
     </div>
   );
