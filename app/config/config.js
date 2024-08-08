2d1f5f4ed081cc4ed919502bbd03ddeb98e9b057
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-08-08 16:58:22 +0530

    configuration

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,9 @@
-// Website Routes
+import { useTranslations } from "next-intl";
 
+export const ApiUrl = "https://e-requirement-backend-v2.vercel.app/api/v2";
+export const BaseUrl = "https://e-requirement-backend-v2.vercel.app/";
+
+// Website Routes
 export const routes = [
   {
     id: 1,
@@ -28,5 +32,33 @@ export const routes = [
   },
 ];
 
-export const ApiUrl = "https://e-requirement-backend-v2.vercel.app/api/v2";
-export const BaseUrl = "https://e-requirement-backend-v2.vercel.app/";
+// breakpoints for swiper
+export const SwiperBreakPoints = {
+  300: {
+    slidesPerView: 1,
+    spaceBetween: 20,
+  },
+  440: {
+    slidesPerView: 2,
+    spaceBetween: 20,
+  },
+  640: {
+    slidesPerView: 3,
+    spaceBetween: 30,
+  },
+  840: {
+    slidesPerView: 4,
+    spaceBetween: 40,
+  },
+  1024: {
+    slidesPerView: 5,
+    spaceBetween: 50,
+  },
+};
+
+export function MyTranslation(translationkey) {
+  const t = useTranslations(translationkey);
+  return t;
+}
+
+export const myPrimaryColor = "#ff9a00";
