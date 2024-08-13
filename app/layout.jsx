7928539dacffb5a35e5de9367096aad4d815a575
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-08-13 12:14:40 +0530

    update layout add internationalization client component in root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,13 @@
-export default function RootLayout({ children }) {
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+
+// client side setup internationalization
+export default async function RootLayout({ children }) {
+  const messages = await getMessages();
+
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <NextIntlClientProvider messages={messages}>
+      {children}
+    </NextIntlClientProvider>
   );
 }
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,7 +3,6 @@ export default function NotFound() {
     <html>
       <body className="flex justify-center items-center h-screen w-screen">
         <h1 className="font-semibold">404 Not Found!!!</h1>
-        
       </body>
     </html>
   );
