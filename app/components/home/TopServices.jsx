9e49639ad8d1308803b9f96492e3671e07dcb953
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-08-13 15:40:57 +0530

    top categories and services

--- app/components/home/TopServices.jsx
+++ app/components/home/TopServices.jsx
@@ -4,19 +4,20 @@ import React from "react";
 import ServiceCard from "@/app/common/ServiceCard";
 import MySkeleton from "@/app/common/MySkeleton";
 import { Grid } from "@mui/material";
+import MyTitle from "@/app/common/MyTitle";
 
 const TopServices = ({ data, loading }) => {
   console.log("Services", data);
   return (
     <div>
-      <h1 className="text-2xl font-semibold my-4">Top Services</h1>
+      <MyTitle title={"Top Services"} />
       <div className="services">
         <Grid container>
           {loading ? (
             <MySkeleton variant={"rectangular"} height={200} width={200} />
           ) : (
-            data.slice(0, 6).map((service) => (
-              <Grid item xs={12} sm={6} md={2}>
+            data.slice(0, 6).map((service, index) => (
+              <Grid key={service.id || index} item xs={12} sm={6} md={2}>
                 <ServiceCard service={service} />
               </Grid>
             ))
