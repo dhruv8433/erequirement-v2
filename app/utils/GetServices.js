bab2e3c96efe32f757b537c6d81df2c715e90717
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-08-13 22:34:30 +0530

    service detailed page

--- app/utils/GetServices.js
+++ app/utils/GetServices.js
@@ -1,13 +1,19 @@
 import { httpAxios } from "../httpAxios";
 
+// Particular provider servics
+export async function getProviderServices(id) {
+  const response = await httpAxios.get(`/get-services/provider/${id}`);
+  return response.data;
+}
+
 // utils for getting services
 export async function getServices() {
   const response = await httpAxios.get("/get-services");
   return response.data;
 }
 
-// Particular provider servics
-export async function getProviderServices(id) {
-  const response = await httpAxios.get(`/get-services/provider/${id}`);
+// particular service
+export async function getSingleService(id) {
+  const response = await httpAxios.get(`/get-services/${id}`);
   return response.data;
 }
