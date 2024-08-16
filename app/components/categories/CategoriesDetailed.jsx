107feb95a5648a1c61b0640f1406b3b28d169299
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-08-16 11:20:58 +0530

    category detailed page

--- app/components/categories/CategoriesDetailed.jsx
+++ app/components/categories/CategoriesDetailed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import CategoryCard from "@/app/common/CategoryCard";
+import { CategorySkeleton } from "@/app/common/CustomSkeleton";
+import { Grid } from "@mui/material";
+import React from "react";
+import TopServices from "../home/TopServices";
+import ServiceCard from "@/app/common/ServiceCard";
+import SubCategryCard from "@/app/common/SubCategryCard";
 
-const CategoriesDetailed = () => {
+const CategoriesDetailed = ({ subCategory, id, loading }) => {
   return (
-    <div>CategoriesDetailed</div>
-  )
-}
+    <div>
+      <Grid container>
+        {loading
+          ? Array.from({ length: 8 }).map((_, index) => (
+              <Grid item xs={12} sm={6} md={3}>
+                <CategorySkeleton key={_} />
+              </Grid>
+            ))
+          : subCategory.map((category) => {
+              if (category.categoryId == id)
+                return (
+                  <Grid item xs={12} sm={6} md={3}>
+                    <div>
+                      <SubCategryCard subcategory={category} />
+                    </div>
+                  </Grid>
+                );
+            })}
+      </Grid>
+    </div>
+  );
+};
 
-export default CategoriesDetailed
+export default CategoriesDetailed;
