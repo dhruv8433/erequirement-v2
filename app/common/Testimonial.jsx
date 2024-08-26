6961d6a45153bf3d102e492132252d6d02afbbcb
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-08-26 11:29:03 +0530

    update testimonials

--- app/common/Testimonial.jsx
+++ app/common/Testimonial.jsx
@@ -1,25 +1,29 @@
-import { Avatar, Rating } from "@mui/material";
 import React from "react";
+import { Avatar, Rating } from "@mui/material";
+import { MyCardBox } from "../custom/MyBox";
+import { MyPrimaryText, MySecondaryText } from "../custom/MyText";
 
 const Testimonial = ({ testimonial }) => {
   const { customer_name, rating, review_title, review_text, review_date } =
     testimonial;
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-6 my-4 max-w-md mx-auto h-[240px]">
+    <MyCardBox className="shadow-lg rounded-lg p-6 my-4 max-w-md mx-auto h-[240px]">
       <div className="flex items-center mb-4">
         <Avatar sizes="small" />
         <div className="ml-3">
-          <h3 className="font-semibold text-md text-gray-900">
-            {customer_name}
-          </h3>
-          <p className="text-sm text-gray-400">
-            {new Date(review_date).toLocaleDateString()}
-          </p>
+          <MyPrimaryText
+            className="font-semibold text-md"
+            title={customer_name}
+          />
+          <MyPrimaryText
+            className="text-sm text-gray-400"
+            title={new Date(review_date).toLocaleDateString()}
+          />
         </div>
       </div>
       <div className="mb-4">
-        <h4 className="font-bold text-md text-gray-800">{review_title}</h4>
+        <MySecondaryText className="font-bold text-md" title={review_title} />
         {/* Rating component */}
         <Rating
           name="read-only"
@@ -29,13 +33,16 @@ const Testimonial = ({ testimonial }) => {
           size="small"
           className="mb-2"
         />
-        <p className="text-gray-700 max-w-[200ch]">
-          {review_text.length > 60
-            ? `${review_text.slice(0, 60)}...`
-            : review_text}
-        </p>
+        <MySecondaryText
+          className=" max-w-[200ch]"
+          title={
+            review_text.length > 60
+              ? `${review_text.slice(0, 60)}...`
+              : review_text
+          }
+        />
       </div>
-    </div>
+    </MyCardBox>
   );
 };
 
