bc273a710404464b9f4ec3dc0a28a37c7bdba31c
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-09-04 11:45:17 +0530

    address reducers

--- app/reducer/rootReducer.js
+++ app/reducer/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import ThemeReducer from "./ThemeReducer";
 import authReducer from "./authReducer";
+import addressReducer from "./addressReducer";
 
 export const rootReducer = combineReducers({
   theme: ThemeReducer,
   auth: authReducer,
+  address: addressReducer,
 });
