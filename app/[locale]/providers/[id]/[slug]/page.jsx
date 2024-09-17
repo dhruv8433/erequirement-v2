b7fd2a31158497c438c8a5515a389dc4173e4c0d
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-09-17 13:02:57 +0530

    provider detailed page

--- app/[locale]/providers/[id]/[slug]/page.jsx
+++ app/[locale]/providers/[id]/[slug]/page.jsx
@@ -1,47 +1,10 @@
 "use client";
 
-import { useParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import { getSignleProvider } from "@/app/utils/GetProviders";
-import ProviderDetailed from "@/app/components/providers/ProviderDetailed";
-import { getProviderServices } from "@/app/utils/GetServices";
+import React from "react";
+import ProviderServices from "@/app/components/providers/ProviderServices";
 
 const page = () => {
-  const { id } = useParams();
-  const [provider, setProvider] = useState({});
-  const [services, setServices] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [serviceLoading, setServiceLoading] = useState(true);
-
-  async function fetchProvider() {
-    try {
-      // provider
-      const response = await getSignleProvider(id);
-      console.log("Provider Fetch Success", response.data);
-      setProvider(response.data);
-      setLoading(false);
-      // service
-      const serviceData = await getProviderServices(id);
-      console.log("Provider Fetch Success", serviceData.data);
-      setServices(serviceData.data);
-      setServiceLoading(false);
-    } catch (error) {
-      console.log("Error in getting provider", error);
-    }
-  }
-
-  useEffect(() => {
-    fetchProvider();
-  }, []);
-
-  return (
-    <ProviderDetailed
-      loading={loading}
-      provider={provider}
-      serviceLoading={serviceLoading}
-      services={services}
-    />
-  );
+  return <ProviderServices />;
 };
 
 export default page;
