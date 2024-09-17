56532b6a3b7ecec2aa8359c55ab2b5bc53b0e57b
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-09-17 13:03:53 +0530

    categories detailed page

--- app/components/categories/CategoriesDetailed.jsx
+++ app/components/categories/CategoriesDetailed.jsx
@@ -3,48 +3,49 @@ import Link from "next/link";
 import { Grid } from "@mui/material";
 import { useLocale } from "next-intl";
 import MySkeleton from "@/app/custom/MySkeleton";
+import NoResult from "@/app/assets/no-result.png";
 import ServiceCard from "@/app/common/ServiceCard";
-import { MyHeading } from "@/app/custom/MyText";
+import { MyPrimaryText } from "@/app/custom/MyText";
 
-const CategoriesDetailed = ({ subCategory, id, loading, slug, title }) => {
+const CategoriesDetailed = ({ subCategory, id, loading, slug, error }) => {
   const locale = useLocale();
   return (
     <div className="my-4 min-h-[500px]">
-      <MyHeading title={title} className={"my-4 text-2xl font-semibold"} />
-      <Grid container spacing={3}>
-        {loading ? (
-          Array.from({ length: 6 }).map((_, index) => (
-            <Grid key={index} item xs={12} sm={6} md={2}>
-              <MySkeleton
-                variant={"rectangular"}
-                height={200}
-                width={200}
-                className={"my-2 rounded-2xl"}
-              />
-            </Grid>
-          ))
-        ) : subCategory ? (
-          subCategory.map((category) => {
-            if (category.categoryId == id)
-              return (
-                <Grid item xs={12} sm={4} md={2}>
-                  <Link
-                    href={`/${locale}/categories/${id}/${slug}/sub-category/${category.id}/${category.slug}`}
-                  >
-                    <div>
-                      <ServiceCard service={category} />
-                    </div>
-                  </Link>
+      {error.statusCode !== 404 ? (
+        <Grid container spacing={3}>
+          {loading
+            ? Array.from({ length: 6 }).map((_, index) => (
+                <Grid key={index} item xs={12} sm={6} md={2}>
+                  <MySkeleton
+                    variant={"rectangular"}
+                    height={200}
+                    width={200}
+                    className={"my-2 rounded-2xl"}
+                  />
                 </Grid>
-              );
-            else {
-              <h1>Not found </h1>;
-            }
-          })
-        ) : (
-          <h1>Not found</h1>
-        )}
-      </Grid>
+              ))
+            : subCategory &&
+              subCategory.map((category) => {
+                if (category.categoryId == id)
+                  return (
+                    <Grid item xs={12} sm={4} md={2}>
+                      <Link
+                        href={`/${locale}/categories/${id}/${slug}/sub-category/${category._id}/${category.slug}`}
+                      >
+                        <div>
+                          <ServiceCard service={category} />
+                        </div>
+                      </Link>
+                    </Grid>
+                  );
+              })}
+        </Grid>
+      ) : (
+        <div className="flex justify-center items-center flex-col my-10">
+          <img src={NoResult.src} alt="" />
+          <MyPrimaryText title={"No result found"} className={"text-xl"} />
+        </div>
+      )}
     </div>
   );
 };
