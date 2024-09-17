f5468a4c4c280dfbff152f1a86db04cc0c3764ef
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-09-17 13:04:21 +0530

    minot changes and skeleton

--- app/custom/CustomSkeleton.jsx
+++ app/custom/CustomSkeleton.jsx
@@ -1,4 +1,6 @@
 import { Divider, Grid, Rating, Skeleton } from "@mui/material";
+import { MyCardBox } from "./MyBox";
+import { ChevronRight } from "@mui/icons-material";
 
 // Provider Skeletons
 export const ProviderSkeletons = () => {
@@ -144,5 +146,136 @@ export const ServiceDetailedSkeleton = () => {
         <ServiceFaqsSkeleton />
       </Grid>
     </Grid>
+  );
+};
+
+// provider detailed skeleton
+export const ProviderDetailedSkeleton = () => {
+  return (
+    <MyCardBox className="m-2 rounded-2xl border border-gray-300 overflow-hidden">
+      {/* Skeleton for banner image */}
+      <Skeleton
+        variant="rectangular"
+        width="100%"
+        height={300}
+        animation="wave"
+      />
+
+      {/* Skeleton for logo image */}
+      <div className="flex justify-center relative -top-10">
+        <Skeleton
+          variant="circular"
+          width={100}
+          height={100}
+          animation="wave"
+        />
+      </div>
+
+      {/* Skeleton for provider info */}
+      <div className="my-1 text-center relative bottom-8">
+        <Skeleton
+          variant="text"
+          width="75%"
+          height={40}
+          animation="wave"
+          style={{ margin: "auto" }}
+        />
+        <Skeleton
+          variant="text"
+          width="60%"
+          height={30}
+          animation="wave"
+          style={{ margin: "auto" }}
+        />
+        <Skeleton
+          variant="text"
+          width="60%"
+          height={30}
+          animation="wave"
+          style={{ margin: "auto" }}
+        />
+      </div>
+    </MyCardBox>
+  );
+};
+
+// breadcrumb Skeleton
+export const BreadCrumbSkeleton = () => {
+  return (
+    <div className="breadcrumb-skeleton my-4">
+      {/* Skeleton for the title */}
+      <Skeleton variant="text" width="20%" height={40} animation="wave" />
+
+      {/* Skeleton for breadcrumbs */}
+      <div className="mt-2 flex gap-2 items-center">
+        <Skeleton
+          variant="text"
+          width="5%"
+          height={30}
+          animation="wave"
+          className="inline-block mr-2"
+        />
+        <ChevronRight />
+        <Skeleton
+          variant="text"
+          width="5%"
+          height={30}
+          animation="wave"
+          className="inline-block mr-2"
+        />
+        <ChevronRight />
+        <Skeleton
+          variant="text"
+          width="10%"
+          height={30}
+          animation="wave"
+          className="inline-block"
+        />
+      </div>
+    </div>
+  );
+};
+
+// provider service skeleton
+export const ProviderServiceSkeleton = () => {
+  return (
+    <div className="border rounded-2xl">
+      {/* Skeleton for Service Image */}
+      <div className="serviceImg h-[200px] w-full flex-shrink-0">
+        <Skeleton
+          variant="rectangular"
+          width="100%"
+          height={200}
+          animation="wave"
+        />
+      </div>
+
+      {/* Skeleton for Card Content */}
+      <div className="p-4 flex-1 flex flex-col justify-between">
+        <div>
+          {/* Skeleton for Service Name */}
+          <Skeleton variant="text" width="80%" height={25} animation="wave" />
+
+          {/* Skeleton for Short Description */}
+          <Skeleton variant="text" width="90%" height={20} animation="wave" />
+        </div>
+
+        {/* Skeleton for Rating and Add to Cart */}
+        <div className="flex justify-between items-center mt-4">
+          <Skeleton
+            variant="rectangular"
+            width={70}
+            height={30}
+            animation="wave"
+          />
+          <Skeleton
+            variant="rectangular"
+            width={100}
+            height={30}
+            animation="wave"
+          />
+        </div>
+      </div>
+    </div>
   );
 };
