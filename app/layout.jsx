cf4a80bdcc40ba9eb73d00916ba630f03a66b86b
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-09-20 17:14:55 +0530

    layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export default async function RootLayout({ children, params }) {
   }
 
   console.log("messages", messages);
-
+  unstable_setRequestLocale(locale);
   return (
     <html lang={locale}>
       <body>
