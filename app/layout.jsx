2bd92a5c2da7184d5501b1a7525413f7fff6dd3f
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-09-20 17:31:58 +0530

    update layout with locale

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,19 +2,19 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages, unstable_setRequestLocale } from "next-intl/server";
 
 export default async function RootLayout({ children, params }) {
-  const locale = params?.locale || "en";
+  const locale = (await params?.locale) || "en";
   console.log("Current Locale:", locale);
 
   let messages;
   try {
     messages = await getMessages(locale);
+    unstable_setRequestLocale(locale);
   } catch (error) {
     console.error("Error loading messages:", error);
     messages = await getMessages("en"); // Fallback to default messages
   }
 
   console.log("messages", messages);
-  unstable_setRequestLocale(locale);
   return (
     <html lang={locale}>
       <body>
