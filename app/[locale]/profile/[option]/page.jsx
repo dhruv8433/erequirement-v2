0c46d199802c5c410645ce4fb7cfc295447cfb8e
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-09-24 13:42:19 +0530

    add user reviews page

--- app/[locale]/profile/[option]/page.jsx
+++ app/[locale]/profile/[option]/page.jsx
@@ -1,17 +1,14 @@
 "use client";
 
+import { useLocale } from "next-intl";
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/navigation";
 import { useParams } from "next/navigation";
 import Orders from "@/app/components/profile/Orders";
-import { useSelector } from "react-redux";
-import Address from "@/app/components/cart/Address";
-import { Modal } from "@mui/material";
-import AddressModal from "@/app/model/AddressModal";
-import { MyCardBox } from "@/app/custom/MyBox";
-import MyModal from "@/app/custom/MyModal";
-import { useRouter } from "next/navigation";
-import { useLocale } from "next-intl";
 import WishLists from "@/app/components/profile/WishLists";
+import ProfileAddress from "@/app/components/profile/ProfileAddress";
+import ProfileReviews from "@/app/components/profile/ProfileReviews";
 
 const page = () => {
   const { option } = useParams();
@@ -35,31 +32,22 @@ const page = () => {
   } else if (option === "addresses") {
     // address management
     return (
-      <>
-        <Address
-          user={userData._id}
-          selectedAddress={selectedAddress}
-          setAddressModal={openAddressModal}
-          setSelectedAddress={setSelectedAddress}
-          isProfilePage={true}
-        />
-        <MyModal open={addressModal} setOpen={setAddressModal}>
-          <MyCardBox
-            width={{ xs: "300px", md: "500px" }}
-            className="rounded-2xl"
-          >
-            <AddressModal
-              user={userData}
-              address={initialData}
-              setAddressModal={setAddressModal}
-            />
-          </MyCardBox>
-        </MyModal>
-      </>
+      <ProfileAddress
+        addressModal={addressModal}
+        initialData={initialData}
+        openAddressModal={openAddressModal}
+        selectedAddress={selectedAddress}
+        setAddressModal={setAddressModal}
+        setSelectedAddress={setSelectedAddress}
+        userData={userData}
+      />
     );
   } else if (option === "wishlists") {
     // whishlist services
     return <WishLists />;
+  } else if (option === "reviews") {
+    // reviews
+    return <ProfileReviews />;
   } else {
     router.replace(`/${locale}/profile`);
   }
