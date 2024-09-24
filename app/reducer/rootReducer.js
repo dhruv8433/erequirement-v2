f3167e147458456544396b24dca73ac1ebe05010
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-09-24 12:06:54 +0530

    add reducer for wishlist

--- app/reducer/rootReducer.js
+++ app/reducer/rootReducer.js
@@ -3,10 +3,12 @@ import ThemeReducer from "./ThemeReducer";
 import authReducer from "./authReducer";
 import addressReducer from "./addressReducer";
 import cartReducer from "./cartReducer";
+import wishlistReducer from "./wishlistReducer";
 
 export const rootReducer = combineReducers({
   theme: ThemeReducer,
   auth: authReducer,
   address: addressReducer,
   cart: cartReducer,
+  wishlist: wishlistReducer,
 });
