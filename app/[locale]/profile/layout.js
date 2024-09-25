189b76b3993c51bc8adb0f5faabb04b235e612b3
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-09-25 09:49:21 +0530

    update layout

--- app/[locale]/profile/layout.js
+++ app/[locale]/profile/layout.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Grid } from "@mui/material";
 import ProfileNavigation from "@/app/components/profile/ProfileNavigation";
 
-const layout = ({ children }) => {
+const Layout = ({ children }) => {
   return (
     <div className="my-10 profile">
-      <Grid container spacing={2}>
-        <Grid itme xs={12} md={4}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={4}>
           <ProfileNavigation />
         </Grid>
         <Grid item xs={12} md={8}>
@@ -17,4 +17,4 @@ const layout = ({ children }) => {
   );
 };
 
-export default layout;
+export default Layout;
