2104c4488ce363aaceb29409c3cb8c0841fa80b7
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-09-29 16:54:43 +0530

    contact us page aos completed

--- app/components/contact/Contact.jsx
+++ app/components/contact/Contact.jsx
@@ -30,11 +30,13 @@ const Contact = () => {
   };
 
   return (
-    <div>
+    <div data-aos="fade-up">
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
           <MySecondaryBox className="p-4 rounded-2xl">
-            <h1 className="text-6xl text-wrap font-semibold">Get In Touch!</h1>
+            <h1 className="text-6xl text-wrap font-semibold" data-aos="fade-up">
+              Get In Touch!
+            </h1>
             <div className="form my-10">
               <form onSubmit={handleSubmit}>
                 <div className="my-3 rounded">
@@ -90,7 +92,7 @@ const Contact = () => {
           </MySecondaryBox>
         </Grid>
         <Grid item xs={12} md={6}>
-          <Grid item xs={12}>
+          <Grid item xs={12} data-aos="fade-up">
             <Spline
               scene="https://prod.spline.design/k1vod-zEg4plR6Ce/scene.splinecode"
               className="rounded-2xl"
@@ -98,34 +100,34 @@ const Contact = () => {
           </Grid>
           <Grid item xs={12}>
             <MyCardBox className="my-5 p-3 rounded-2xl">
-              <div className="info my-2">
+              <div className="info my-2" data-aos="fade-up">
                 <div className="flex items-center gap-5">
                   <div className="icon p-2 primary-bg rounded">
                     <PlaceOutlined className="text-white" />
                   </div>
-                  <div className="info">
+                  <div className="info" data-aos="fade-up">
                     <h1 className="text-xl font-semibold">Location</h1>
                     <p className="text-lg">Las Vegas, NV, USA</p>
                   </div>
                 </div>
               </div>
-              <div className="info my-2">
+              <div className="info my-2" data-aos="fade-up">
                 <div className="flex items-center gap-5">
                   <div className="icon p-2 primary-bg rounded">
                     <EmailOutlined className="text-white" />
                   </div>
-                  <div className="info">
+                  <div className="info" data-aos="fade-up">
                     <h1 className="text-xl font-semibold">Email</h1>
                     <p className="text-lg">erequirement-support@gmail.com</p>
                   </div>
                 </div>
               </div>
-              <div className="info my-2">
+              <div className="info my-2" data-aos="fade-up">
                 <div className="flex items-center gap-5">
                   <div className="icon p-2 primary-bg rounded">
                     <PhoneOutlined className="text-white" />
                   </div>
-                  <div className="info">
+                  <div className="info" data-aos="fade-up">
                     <h1 className="text-xl font-semibold">Phone</h1>
                     <p className="text-lg">+1 1234567890</p>
                   </div>
@@ -135,7 +137,7 @@ const Contact = () => {
           </Grid>
         </Grid>
       </Grid>
-      <div className="map my-6">
+      <div className="map my-6" data-aos="zoom-out">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206252.80376346345!2d-115.33980707700024!3d36.125164460739!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80beb782a4f57dd1%3A0x3accd5e6d5b379a3!2sLas%20Vegas%2C%20NV%2C%20USA!5e0!3m2!1sen!2sin!4v1725680446186!5m2!1sen!2sin"
           width="100%"
