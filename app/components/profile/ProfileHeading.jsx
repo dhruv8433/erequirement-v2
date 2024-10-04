3ee4cb3a79a4d9133be32c06c14fc7911cdd9cc4
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-10-04 13:44:49 +0530

    notification action buttons

--- app/components/profile/ProfileHeading.jsx
+++ app/components/profile/ProfileHeading.jsx
@@ -3,12 +3,18 @@ import { Divider } from "@mui/material";
 import { MyHeading } from "@/app/custom/MyText";
 import { MyBorderdButton } from "@/app/custom/MyButton";
 
-const ProfileHeading = ({ heading, isAction, actionTitle }) => {
+const ProfileHeading = ({ heading, isAction, actionTitle, actionEvent }) => {
   return (
     <div className="mb-1" data-aos="fade-up">
       <div className="flex justify-between items-center">
         <MyHeading title={heading} className={"text-2xl font-semibold my-1"} />
-        {isAction && <MyBorderdButton title={actionTitle} className={'py-1 px-2 mb-1'}/>}
+        {isAction && (
+          <MyBorderdButton
+            title={actionTitle}
+            className={"py-1 px-2 mb-1"}
+            onClickFunction={actionEvent}
+          />
+        )}
       </div>
       <Divider />
     </div>
