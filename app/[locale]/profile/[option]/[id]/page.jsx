65815074fc426ef7ce48b4692d6e3defd151ad12
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-10-06 10:48:49 +0530

    update profile page

--- app/[locale]/profile/[option]/[id]/page.jsx
+++ app/[locale]/profile/[option]/[id]/page.jsx
@@ -6,9 +6,10 @@ import { useRouter } from "next/navigation";
 import { useParams } from "next/navigation";
 import { MyCardBox } from "@/app/custom/MyBox";
 import ProfileHeading from "@/app/components/profile/ProfileHeading";
+import OrderDetailed from "@/app/components/profile/OrderDetailed";
 
 const page = () => {
-  const { option } = useParams();
+  const { option, id } = useParams();
   console.log(option);
 
   const router = useRouter();
@@ -23,6 +24,8 @@ const page = () => {
   return (
     <MyCardBox className="border p-5 rounded-2xl" data-aos="fade-up">
       <ProfileHeading heading={t("orders_details")} />
+
+      <OrderDetailed orderId={id} />
     </MyCardBox>
   );
 };
