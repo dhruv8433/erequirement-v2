8d193b26f8bb054022886281585e97f568984de3
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-10-07 22:13:50 +0530

    remove user info if user try to do any changes in cookies regarding user authentications

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -9,14 +9,38 @@ import TopServices from "../components/home/TopServices";
 import Categories from "../components/home/TopCategories";
 import TopProviders from "../components/home/TopProviders";
 import Testimonials from "../components/home/Testomonials";
+import { useEffect, useRef } from "react"; // Import useRef
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { logoutFromRedux } from "../reducer/authReducer";
 
 export default function Home() {
   const t = useTranslations("home");
 
-  // custom hook that return whole home screen
+  // Custom hook that returns the whole home screen
   const { data, loading } = useHomeScreen();
+  const dispatch = useDispatch();
+  const toastShownRef = useRef(false); // Create a ref to track toast display
 
   document.title = `Home | ${WebName}`;
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const paramValue = params.get("error");
+
+    // Check if the error is "not_authenticated" and if the toast hasn't been shown yet
+    if (paramValue === "not_authenticated" && !toastShownRef.current) {
+      console.log("not_authenticated");
+      toast.error("Please login to continue");
+      dispatch(logoutFromRedux());
+      toastShownRef.current = true; // Mark the toast as shown
+    }
+
+    // If paramValue is not "not_authenticated", reset the ref to allow showing the toast again on next error
+    if (paramValue !== "not_authenticated") {
+      toastShownRef.current = false;
+    }
+  }, [dispatch]); // No need to include paramValue in dependencies
 
   return (
     <div>
@@ -26,9 +50,9 @@ export default function Home() {
         <Slider data={data?.swiper} loading={loading} />
         <TopProviders data={data?.top_categories} loading={loading} />
         <TopServices data={data?.top_services} loading={loading} />
-        {/* subscriber */}
+        {/* Subscriber */}
         <Subscribe t={t} />
-        {/* Testomonials */}
+        {/* Testimonials */}
         <Testimonials testimonials={data?.customer_reviews} loading={loading} />
       </div>
     </div>
