69f6302f1ab695a35dc513f62961928508d78045
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-03-28 12:37:09 +0530

    update ads

--- app/common/Advertisement.jsx
+++ app/common/Advertisement.jsx
@@ -1,18 +1,20 @@
 import { Box } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 const Advertisement = ({ ads }) => {
   return (
     <Box height={{ xs: "230px", md: "500px" }}>
-      <img
-        src={ads.image}
-        height={"100%"}
-        className="object-fill aspect-video h-full w-full "
-        width={"100%"}
-        alt=""
-        loading="lazy"
-      />
-      ;
+      <Link href={ads.href}>
+        <img
+          src={ads.slider_image}
+          height={"100%"}
+          className="object-fill aspect-video h-full w-full "
+          width={"100%"}
+          alt=""
+          loading="lazy"
+        />
+      </Link>
     </Box>
   );
 };
